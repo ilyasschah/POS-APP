6f4dda48736434d90dd3db8fc8ffa6d1fac26c80
ilyass chah <ilyasschah18@gmail.com>
2025-10-08 23:12:21 +0100

    local_db

--- Web-POS.Api/obj/Debug/net9.0/Web-POS.Api.AssemblyInfo.cs
+++ Web-POS.Api/obj/Debug/net9.0/Web-POS.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web-POS.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ad2a61743563501b759b500ffc98fc0eabc9b89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b10c861914fa9c6639759f190c973be9a1bec972")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web-POS.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web-POS.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
